d87551d96048452b32c2eae3bf4dd240d4c20292
Vladimir Molotov <molotov.vn@gmail.com>
2019-01-31 22:21:49 +0200

    more hashes in file, counts, UI, log, some features

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import re
 
 # python main.py md5 hash.txt dict.txt
 
+hash_count, good, bad = 0, 0, 0
+logname = datetime.now().strftime('%Y_%m_%d') + '.log'
+
 try:
 	algrtm, hashfile, dictfile = argv[1], argv[2], argv[3]
 except IndexError:
@@ -13,30 +16,24 @@ except IndexError:
 
 
 def write_result(chash, cpass):
-	with open('result.txt', 'a') as result:
+	with open(logname, 'a') as result:
 		line = str(datetime.now()) + ' - ' + chash + ':' + cpass + '\n'
 		result.writelines(line)
 
 
-with open(hashfile) as file:
-	workhash = file.read()
-	workhash = workhash.replace('\n', '')
-	if re.match(r'^[0-9a-f]{32}$', workhash) is None:
-		print('This hash is Invalid! -', workhash)
-		write_result(workhash, cpass='[INVALID HASH]')
-		raise SystemExit
-
-
 def generator(string):
+	global good
 	status = '[Not Found]'
 	for word in string:
 		password = word.replace('\n', '')
 		if encryption(password) == workhash:
-			yield '-'*20 + '\nFIND! - ' + password
+			yield '[FIND]'
+			good += 1
 			status = password
 			break
-		else:
-			yield '[NO]' + password
+		# else:
+		# 	yield '[NO]' + password
+	print(workhash + ':' + status)
 	return write_result(workhash, status)
 
 
@@ -46,8 +43,17 @@ def encryption(string):
 	return signature
 
 
-print()
-with open(dictfile, errors='ignore') as dic:
-	for line in generator(dic):
-		print(line)
-print()
+with open(hashfile) as file:
+	for workhash in file:
+		hash_count += 1
+		workhash = workhash.replace('\n', '')
+		if re.match(r'^[0-9a-f]{32}$', workhash) is None:
+			print(workhash + ':' + '[Invalid Hash]')
+			write_result(workhash, cpass='[INVALID HASH]')
+			bad += 1
+		else:
+			with open(dictfile, errors='ignore') as dic:
+				for line in generator(dic):
+					print(line)
+	print('[Hashes]', '\nTotal:\t', hash_count, '\nFinded:\t', good, '\nInvalid:', bad)
+	print('All detail results save into:', logname)
